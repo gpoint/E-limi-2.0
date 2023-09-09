ef7ab601c6475067aa95d013ec1fbae5fc439a2b
Chibueze Favour Harry <buezeharry@gmail.com>
2023-09-10 00:57:24 +0100

    update: add progress bar

--- src/main.js
+++ src/main.js
@@ -6,10 +6,28 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import VueProgressBar from "@aacassandra/vue3-progressbar";
+
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+
+const progressBarOptions = {
+  color: "#77132b",
+  failedColor: "#ff0000",
+  thickness: "3px",
+  transition: {
+    speed: "0.2s",
+    opacity: "1",
+    termination: 300,
+  },
+  autoRevert: true,
+  location: "top",
+  inverse: false,
+};
+
+app.use(VueProgressBar, progressBarOptions)
 
 app.mount("#app");
